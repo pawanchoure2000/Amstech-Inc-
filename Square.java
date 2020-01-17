5407507faaffb07d334613087dea26eb7a2f7562
pawan choure <52540649+pawanchoure2000@users.noreply.github.com>
2020-01-17 18:52:35 +0530

    THESE 5 FILES ARE PLATTER IS-A

---
+++ Square.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package platterisa;
+
+/**
+ *
+ * @author pawan
+ */
+interface  Square {
+    
+ 
+    public  static  final double side =0.0 ;//compile time constant 
+    
+    double getsquarearea(double side);
+
+    
+    double getsquareperimeter(double side);
+
+    
+    
+}
